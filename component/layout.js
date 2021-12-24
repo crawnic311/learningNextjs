2a3be7499a3e6db8fcd0e312c182acf4b18372f8
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-23 19:05:13 -0700

    Major Layout Added

---
+++ component/layout.js
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import Link from 'next/link'
+import styles from '../styles/Layout.module.css'
+import styles2 from '../styles/Home.module.css'
+
+export default function Layout({ children }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const openMenu = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <>
+      <header class={styles2.header}>
+        <nav class={styles.navbar}>
+          <Link href="/">
+            <a class={styles.navlogo}>[BrandLogo]</a>
+          </Link>
+          <ul
+            class={
+              isOpen === false
+                ? styles.navmenu
+                : styles.navmenu + ' ' + styles.active
+            }
+          >
+            <li class={styles.navitem}>
+              <Link href="/">
+                <a class={styles.navlink}>Home</a>
+              </Link>
+            </li>
+            <li class={styles.navitem}>
+              <Link href="/about">
+                <a class={styles.navlink}>About</a>
+              </Link>
+            </li>
+            <li class={styles.navitem}>
+              <Link href="/contact">
+                <a class={styles.navlink}>Contact</a>
+              </Link>
+            </li>
+          </ul>
+          <button
+            class={
+              isOpen === false
+                ? styles.hamburger
+                : styles.hamburger + ' ' + styles.active
+            }
+            onClick={openMenu}
+          >
+            <span class={styles.bar}></span>
+            <span class={styles.bar}></span>
+            <span class={styles.bar}></span>
+          </button>
+        </nav>
+      </header>
+      {children}
+      <div className={styles2.container}>
+        <footer className={styles.footer}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </a>
+        </footer>
+      </div>
+    </>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,91 +1,11 @@
 import '../styles/globals.css'
-import React, { useState } from 'react'
-import LoginForm from './login/login'
-import HomeHeader from './homepage/homeheader'
-import Homepage from './homepage/homepage'
-import RecipeNav from './homepage/recipenav'
-import CreateRecipe from './create/createrecipe'
-import Navbar from './navbar'
-import { BrowserRouter as Router } from 'react-router-dom'
+import Layout from '../component/layout'
 
 function MyApp({ Component, pageProps }) {
-  const adminUser = {
-    email: 'dillon.craw@gmail.com',
-    password: '123',
-  }
-
-  const [user, setUser] = useState({ name: '', email: '', password: '' })
-  const [error, setError] = useState('')
-  const [nav, setNav] = useState({
-    recipes: true,
-    create: false,
-    nutrition: false,
-    about: false,
-  })
-
-  const Login = (details) => {
-    console.log(details)
-
-    if (
-      details.email == adminUser.email &&
-      details.password == adminUser.password
-    ) {
-      console.log('Logged in')
-      setUser({
-        name: details.name,
-        email: details.email,
-        password: details.password,
-      })
-    } else {
-      console.log('Details do not match!')
-      setError('Details do not match!')
-    }
-  }
-
-  const Logout = () => {
-    console.log('Logout')
-    setUser({
-      name: '',
-      email: '',
-      password: '',
-    })
-  }
-
-  const Navigator = (navigate) => {
-    setNav({})
-  }
-
-  const Create = (recipe) => {
-    console.log(recipe)
-  }
-
   return (
-    <div className="Foody-App">
-      {user.email != '' ? (
-        <>
-          <Router>
-            <Navbar />
-          </Router>
-          <header className="Foody-Header">
-            {/*<HomeHeader />*/}
-            {/*<button onClick={Logout}>Logout</button>*/}
-          </header>
-          {/*<div className="Foody-Header-Homepage">
-            <Homepage />
-            <div className="Recipe-Nav-Wrapper">
-              <RecipeNav />
-            </div>
-      </div> */}
-          <div className="Foody-Create">
-            <CreateRecipe Create={Create} />
-          </div>
-        </>
-      ) : (
-        <div className="Foody-Login">
-          <LoginForm Login={Login} error={error} />
-        </div>
-      )}
-    </div>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
