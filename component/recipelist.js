fbb31e274fbe726135294691e9ac390b6d2a0bc7
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-24 21:52:11 -0700

    CSS for RecipeList turned to styled Modules

--- component/recipelist.js
+++ component/recipelist.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import styles from '../styles/RecipeList.module.css'
 
 const RecipeList = () => {
   return (
-    <div className="Recipe-Holder">
-      <div className="Recipe">
+    <div className={styles.RecipeHolder}>
+      <div className={styles.Recipe}>
         <img
           src="https://images.unsplash.com/photo-1494597564530-871f2b93ac55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1413&q=80"
           alt=""
-          className="Recipe-Image"
+          className={styles.RecipeImage}
         />
-        <div className="Recipe-Description">
+        <div className={styles.RecipeDescription}>
           This is a recipe that tastes so good. You would love to eat it if I
           let you. Maybe one day you will be allowed to eat it.
         </div>
